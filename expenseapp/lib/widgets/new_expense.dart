55369a56de22c72c3ef83b2fd2f66ff61c598fc5
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-11-16 09:51:59 +0300

    calendar

--- expenseapp/lib/widgets/new_expense.dart
+++ expenseapp/lib/widgets/new_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({Key? key}) : super(key: key);
@@ -12,6 +13,40 @@ class _NewExpenseState extends State<NewExpense> {
   // Controller
   final _nameController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _openDatePicker() async {
+    // DatePicker açmak.. ✅
+    // DatePicker'dan gelen değeri Text olarak yazdırmak
+    // built-in function
+
+    // sync => bir işlem bitmeden diğerinin başlamadığı yapılar
+    // async => alt satıra geçmek için işlemin bitmesini beklemezler // await
+    DateTime now = DateTime.now();
+    DateTime oneYearAgo = DateTime(now.year - 1, now.month, now.day);
+
+    // 1 yıl öncesi ve bugün arasında kısıtlama
+    // then => async bir işlemin geri dönüş sağladığı anda çalışacak bloğunu tanımlar.
+    // showDatePicker(
+    //         context: context,
+    //         initialDate: now,
+    //         firstDate: oneYearAgo,
+    //         lastDate: now)
+    //     .then((value) {
+    //   print(value);
+    // });
+
+    // await => ilgili async işlemini bekle
+    DateTime? selectedDate = await showDatePicker(
+        context: context,
+        initialDate: _selectedDate == null ? now : _selectedDate!,
+        firstDate: oneYearAgo,
+        lastDate: now);
+
+    setState(() {
+      _selectedDate = selectedDate;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,13 +66,13 @@ class _NewExpenseState extends State<NewExpense> {
                 const InputDecoration(label: Text("Amount"), prefixText: "₺"),
           ),
           IconButton(
-              onPressed: () {
-                // DatePicker açmak..
-                // DatePicker'dan gelen değeri Text olarak yazdırmak
-              },
+              onPressed: () => _openDatePicker(),
               icon: const Icon(Icons.calendar_month)),
-          const Text(
-              "Tarih Seçiniz.."), // seçilen tarihi formatlayarak yazdırmak..
+          // Ternary Operator
+          Text(_selectedDate == null
+              ? "Tarih Seçiniz"
+              : DateFormat.yMd().format(
+                  _selectedDate!)), // seçilen tarihi formatlayarak yazdırmak..
           ElevatedButton(
               onPressed: () {
                 print("Kayıt başarılı: ${_nameController.text}");
