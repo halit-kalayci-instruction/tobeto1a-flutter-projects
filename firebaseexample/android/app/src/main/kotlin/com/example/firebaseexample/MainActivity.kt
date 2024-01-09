109a5e21dd24c4c23da9e997c07571355401ae76
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-09 10:36:27 +0300

    'firebase'

---
+++ firebaseexample/android/app/src/main/kotlin/com/example/firebaseexample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebaseexample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ firebaseexample/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebaseexample/firebase_options.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MaterialApp());
+}
