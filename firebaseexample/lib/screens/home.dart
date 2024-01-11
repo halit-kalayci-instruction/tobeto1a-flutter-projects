fddde89eded5302fac85c09efa9a873ae5e622a6
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-11 11:34:45 +0300

    firebase storage

--- firebaseexample/lib/screens/home.dart
+++ firebaseexample/lib/screens/home.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 final firebaseAuthInstance = FirebaseAuth.instance;
+final firebaseStorageInstance = FirebaseStorage.instance;
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,6 +16,30 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  File? _pickedFile;
+
+  void _pickImage() async {
+    final image = await ImagePicker()
+        .pickImage(source: ImageSource.camera, imageQuality: 50, maxWidth: 150);
+
+    if (image != null) {
+      setState(() {
+        _pickedFile = File(image.path);
+      });
+    }
+  }
+
+  void _upload() async {
+    final user = firebaseAuthInstance.currentUser;
+    final storageRef =
+        firebaseStorageInstance.ref().child("images").child("${user!.uid}.jpg");
+
+    await storageRef.putFile(_pickedFile!);
+
+    final url = await storageRef.getDownloadURL();
+    print(url);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,8 +53,25 @@ class _HomeState extends State<Home> {
               icon: const Icon(Icons.logout))
         ],
       ),
-      body: const Center(
-        child: Text("Ana Sayfa"),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          CircleAvatar(
+            radius: 40,
+            backgroundColor: Colors.grey,
+            foregroundImage:
+                _pickedFile != null ? FileImage(_pickedFile!) : null,
+          ),
+          TextButton(
+              onPressed: () {
+                _pickImage();
+              },
+              child: const Text("Resim Seç")),
+          ElevatedButton(
+              onPressed: () {
+                _upload();
+              },
+              child: const Text("Yükle"))
+        ]),
       ),
     );
   }
