67ae479a5b574fb7d92dc82a0874f29aeea50101
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-11 10:47:15 +0300

    .

--- firebaseexample/lib/main.dart
+++ firebaseexample/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebaseexample/firebase_options.dart';
 import 'package:firebaseexample/screens/auth.dart';
+import 'package:firebaseexample/screens/home.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -8,7 +10,15 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MaterialApp(
-    home: Auth(),
+  runApp(MaterialApp(
+    home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Home();
+          }
+          return const Auth();
+        }),
   ));
 }
+// StreamBuilder
